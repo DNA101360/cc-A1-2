b6b799fef8784c6d24c180040216395c0b168d5d
Devin Amalean <s3821117@student.rmit.edu.au>
2023-04-07 22:30:14 +1000

    Made the login page the entry point for the application

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Login />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/home" element={<Home />}></Route>
           <Route path="/register" element={<Register/>}></Route>
